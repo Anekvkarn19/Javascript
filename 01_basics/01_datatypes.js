4eba846ddc46becd72954de92ab5729009b99b75
Anekvarna <anek.varna030@gmail.com>
2026-05-21 20:39:06 +0000

    Datatypes

---
+++ 01_basics/01_datatypes.js
@@ -0,0 +1,23 @@
+"use strict"; //treat all JS code as newer version
+//alert(3+3) //using node.js not browser
+console.log(3
+    +3);//code readability should be high
+console.log("Anek");
+
+let name="hitesh";
+let age=15
+let isLoggedIn =false
+let state;
+
+//number => 2 to power 53
+//bigint
+//string=""
+//boolean=>true/false
+//null =>standalone value
+//undefined => 
+//symbol =>unique
+
+//object
+console.log(typeof null);//object
+console.log(typeof undefined)//undefined
+

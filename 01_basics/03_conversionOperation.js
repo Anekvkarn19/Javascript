f2f27f4268e774032853cab2615b3ccd11308831
Anekvarna <anek.varna030@gmail.com>
2026-05-22 19:34:02 +0000

    conversionOperations

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -21,3 +21,39 @@ console.log(booleanIsLoggedIn);
 
 let someNumber =33
  let stringNumber =String(someNumber);
+
+ //*******************Operations******* */
+
+ let value=3
+ let negValue =-value
+ //console.log(negValue);
+ //console.log(2+2);
+ //console.log(2-2);
+ //console.log(2*2);
+ //console.log(2**3);
+ //console.log(2/3);
+ //console.log(2%3);
+
+ let str1="hello"
+ let str2=" anekk"
+
+ let str3=str1 +str2
+//  console.log(str3);
+
+//  console.log("1"+2);//12
+//  console.log(1+ "2");//12
+//  console.log("1"+2+2);//122
+//  console.log(1+2+"2");//32
+
+//  console.log(true);//true
+//  console.log(true+1);//2
+//  //console.log(true+);//error
+//  console.log(+true);//1
+//  console.log(+"");//0
+
+let a=100;
+console.log(++a);
+console.log(a++);
+
+
+

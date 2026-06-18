f3e90acfa426e73366c660ca35746725526dba4d
Anekvarna <anek.varna030@gmail.com>
2026-06-18 07:45:20 +0000

    taking parameters in function

--- 03_basics/01_functions.js
+++ 03_basics/01_functions.js
@@ -28,14 +28,42 @@ const result = addTwoNumbers(3,4)
 function loginUserMessage(username = "sam")
 {
     if(username === undefined){
-        console.log("Please enter a username");
+        // console.log("Please enter a username");
         return;
     }
     return `${username} just logged in`
 }
 
 // console.log(loginUserMessage("anekvarna"))
-console.log(loginUserMessage("anekaa"))
+// console.log(loginUserMessage("anekaa"))
 
+function calculateCartPrice(...num1){//... is called rest operator
+    return num1
+}
+
+// console.log(calculateCartPrice(200, 400, 500))
+
+const user={
+    username: "anekkk",
+    price: 199
+}
+
+function handleObject(anyObject){
+    console.log(`Username is ${anyObject.username} and the price is ${anyObject.price}`)
+}
+
+// handleObject(user)
+handleObject({
+    username:"sam",
+    price: 399
+})
+
+const myNewArray =[200,400,100,600]
+
+function returnSecondValue(getArray){
+    return getArray[1]
+}
+// console.log(returnSecondValue(myNewArray));
+console.log(returnSecondValue([200,400,500,1000]));
 
 

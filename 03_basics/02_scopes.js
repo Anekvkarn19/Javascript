9299b97611645dbb25402c1e5817b5888d03d5ee
Anekvarna <anek.varna030@gmail.com>
2026-06-18 08:05:54 +0000

    scope this and arrow

---
+++ 03_basics/02_scopes.js
@@ -0,0 +1,15 @@
+// var c=300
+// {}//scope
+let a =300
+if(true){
+    let a =10
+    const b=20
+    
+}
+for(let i=0; i<array.length; i++){
+    const element =array[i];
+}
+
+console.log(a);
+// console.log(b);
+// console.log(c);//ye print hojayega kyuki var scope nhi dekhta

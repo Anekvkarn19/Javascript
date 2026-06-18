fdef0c61971496581086db356ae693efa76c9833
Anekvarna <anek.varna030@gmail.com>
2026-06-18 16:24:32 +0000

    scopes

--- 03_basics/02_scopes.js
+++ 03_basics/02_scopes.js
@@ -6,10 +6,56 @@ if(true){
     const b=20
     
 }
-for(let i=0; i<array.length; i++){
-    const element =array[i];
-}
+// for(let i=0; i<array.length; i++){
+//     const element =array[i];
+// }
 
-console.log(a);
+// console.log(a);
 // console.log(b);
 // console.log(c);//ye print hojayega kyuki var scope nhi dekhta
+
+
+function one(){
+    const username ="anekk"
+
+    function two(){
+        const website ="youtube"
+        console.log(username);
+    }
+    // console.log(website);
+
+    two()
+}
+
+one()
+
+if(true)
+{
+    const username ="anekk"
+    if(username ==="anekk"){
+        const website =" youtube";
+        // console.log(username + website);
+    }
+    // console.log(website);//error
+}
+// console.log(username);//error
+
+
+// +++++++++++++interesting ++++++++++++
+
+addone(5)//will not give error
+function addone(num){
+ return num +1
+}
+
+
+
+// addTwo(5)//will give error
+const addTwo =function(num){
+    return num+2
+}
+
+addTwo(5)
+
+
+

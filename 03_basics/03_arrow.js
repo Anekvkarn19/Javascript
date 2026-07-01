03abfd61b57f10331949a11b4cdec378e8173fc5
Anekvarna <anek.varna030@gmail.com>
2026-07-01 17:41:10 +0000

    this and arrow funcion

--- 03_basics/03_arrow.js
+++ 03_basics/03_arrow.js
@@ -13,4 +13,32 @@ const user ={
 // user.username="sam"
 // user.welcomeMessage()
 
-console.log(this);
+// console.log(this);
+
+// function chai(){
+//     let username = "anekk";
+//     console.log(this.username);
+// }
+
+// chai()
+
+const chai = () => {
+    let username = "anekk"
+    console.log(this);
+
+}
+// chai()
+// const addTwo =(num1, num2) => {
+//     return num1 + num2
+// }
+// const addTwo =(num1, num2) => num1 + num2  
+// const addTwo =(num1, num2) => (num1 + num2)
+
+const addTwo =(num1, num2) => ({username: "anekk"})
+    console.log(addTwo(2, 3));
+
+    const myArray = [1, 2, 3, 4, 5]
+
+    myArray.forEach(() => {
+        console.log(this);
+    })

3e715e3fa01bf26a5e05694c1b13f8ddb36c45d2
Anekvarna <anek.varna030@gmail.com>
2026-07-08 11:47:52 +0000

    iterations

---
+++ 05_iterations/four.js
@@ -0,0 +1,26 @@
+const myObject={
+    js:'JavaScript',
+    py:'Python',
+    cpp:'C++',
+    rb:'Ruby'
+}
+
+for(const key in myObject){
+    // console.log(`${key} shortcut is for ${myObject[key]}`);
+}
+
+const programming =["js", "py", "cpp", "rb"];
+
+for(const key in programming){
+    // console.log(programming[key]);
+}
+
+
+const map = new Map();
+map.set("IN","India");
+map.set("USA","United States of America");
+map.set("Fr","France");
+
+for(const key in map){
+    console.log(key);
+}   
---
+++ 05_iterations/two.js
@@ -0,0 +1,22 @@
+
+// let index =0
+// while(index<=10){
+//  console.log(`Value is: ${index}`);
+//  index= index+2;   
+// } 
+
+let myArray=["flash","superman","batman","wonderwoman"]
+
+let index1=0
+// while(index1<myArray.length){
+//     const element = myArray[index1];
+//     console.log(`Element is: ${element}`);
+//     index1++;
+// }
+
+let score =11
+
+do{
+    console.log(`Score is: ${score}`);
+    score++;
+} while(score<=10);
